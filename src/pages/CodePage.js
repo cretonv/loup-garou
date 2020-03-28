fce6ac81521e2806b6f4fcd052acd85a39413488
Vincent <vincecreton@hotmail.fr>
2020-03-28 18:01:56 +0100

    Design partiel d'un premier formulaire

--- src/pages/CodePage.js
+++ src/pages/CodePage.js
@@ -3,6 +3,72 @@ import {useHistory} from 'react-router-dom';
 import {useUser} from '../services/User';
 import Button from '../components/Button';
 import firebase from '../services/Firebase';
+import styled from 'styled-components'
+
+const FormField = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    margin-bottom: .5em;
+    
+        &:last-child {
+            margin-bottom: 0;
+        }
+        
+        label {
+            align-self: center;
+            flex-shrink: 0;
+            flex-basis: 100%;
+            width: 100%;
+            margin-bottom: 10px;
+        }
+        
+        label:after {
+            content: '*';
+            color: red;
+            margin-left: 5px;
+        }
+        
+        > section {
+            border: 1px solid #aaa;
+            border-radius: $radius;
+            display: flex;
+            flex: 1;
+            margin-bottom: 15px;
+            input {
+                border: 0;
+                border-left: 1px solid #ccc;
+                flex-grow: 1;
+                font-size: 1.1rem;
+                font-weight: 300;
+                padding: .35em .5em;
+            }
+        }
+`
+
+const MainDiv = styled.div`
+    align-self: center;
+    background-color: #fefefe;
+    border: 1px solid #f2f2f2;
+    border-radius: 5px;
+    width: 80%;
+    padding: 2rem;
+    margin-bottom: 20px;
+    
+    @media screen and (min-width: 550px) {
+        min-width: 350px;
+        max-width: 450px;
+        box-shadow: 0 30px 60px -30px rgba(0, 0, 0, .5);
+    }
+`
+
+const FormContainer = styled.div`
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+
 
 
 const CodePage = ({setGame}) => {
@@ -41,21 +107,35 @@ const CodePage = ({setGame}) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="code"
-        placeholder="Ajouter le code de la partie"
-        onChange={e => setCode(e.target.value)}
-      />
-      <input
-        type="text"
-        name="name"
-        placeholder="Ajouter votre nom"
-        onChange={e => setName(e.target.value)}
-      />
-      <Button>Démarrer</Button>
-    </form>
+      <FormContainer>
+    <MainDiv>
+        <form onSubmit={handleSubmit}>
+            <FormField>
+                <label htmlFor="field">Code de la partie</label>
+                    <section>
+                          <input
+                            type="text"
+                            name="code"
+                            placeholder="Ajouter le code de la partie"
+                            onChange={e => setCode(e.target.value)}
+                          />
+                    </section>
+            </FormField>
+            <FormField>
+                <label htmlFor="field">Pseudo pour la partie</label>
+                    <section>
+                          <input
+                            type="text"
+                            name="name"
+                            placeholder="Ajouter votre nom"
+                            onChange={e => setName(e.target.value)}
+                          />
+                    </section>
+            </FormField>
+            <Button>Démarrer</Button>
+        </form>
+    </MainDiv>
+      </FormContainer>
   );
 };
 
